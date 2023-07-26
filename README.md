2a77c08cc2b41f2b573dea44d725e8d90b70b9b3
Tom Szumowski <tszumowski@gmail.com>
2023-07-26 08:18:58 -0400

    added bunch of LLM UI and chat stuff

--- README.md
+++ README.md
@@ -3,3 +3,62 @@ Serverless OpenAI+ChatGPT+Pinecone cloud-based pipelines to embed and index all 
 
 TODO: Fill in
 
+# Gradio Application
+
+The `scripts/app` directory contains code to deploy this as a Gradio application.
+
+## Local Run
+You can run locally by creating a virtual environment, installing the pip requirements
+and then running with `python scripts/app/app.py`. See the header of the script
+for setup. Once running, navigate to `http://127.0.0.1:7860/`
+
+You can also build the Dockerfile and run that way:
+
+```
+cd scripts/app
+docker build -t ralm-app .
+docker run -it --rm \
+    -e OPENAI_API_KEY=<openai-api-key> \
+    -e PINECONE_API_KEY=<pinecone-api-key> \
+    -p 7860:7860 \
+    ralm-app \
+    python app.py \
+    --address=0.0.0.0 \
+    --port=7860 \
+    --username=<username> \
+    --password=<password>
+```
+Fill in `<>` field as desired. Run. Then navigate to `http://127.0.0.1:<port>`.
+
+## Deploying
+
+You can follow the guide [here](https://medium.com/@tszumowski/vocaltales-going-the-extra-mile-adventures-in-deploying-a-chatgpt-openai-app-the-hard-way-b566d1bbc6fa)
+for multiple ways of deploying.
+
+In this example we will push to GCP and deploy via Cloud Build via authentication.
+
+First, build with:
+```
+docker tag ralm-app gcr.io/<project-id>/ralm-app
+docker push gcr.io/<project-id>/ralm-app
+```
+
+(Here, ralm stands for "retrieval augmented language model")
+
+Then deploy with:
+```
+gcloud run deploy ralm-app \
+    --image gcr.io/<project-id>/ralm-app \
+    --platform managed \
+    --set-env-vars=OPENAI_API_KEY=<openai-key-string>,PINECONE_API_KEY=<pinecone-api-key> \
+    --allow-unauthenticated \
+    --port=7860 \
+    --cpu=1 \
+    --memory=512Mi \
+    --min-instances=0 \
+    --max-instances=3 \
+    --command="python" \
+    --args="app.py,--address=0.0.0.0,--port=7860,--username=<username>,--password=<password>"
+```
+
+Don't forget to fill in the `<>` fields.
--- scripts/app/app.py
+++ scripts/app/app.py
@@ -6,14 +6,26 @@ Setup:
 - Set OPENAI_API_KEY and PINECONE_API_KEY env variables
 - Configure the parameters in USER CONFIG below
 
+Notes:
+- This is just a proof-of-concept. Some significant prompt-engineering would be
+    needed to make this work well.
+- This sends history in and out of the chatbot, but it is not a conversational agent,
+    so it doesn't use it. The history is in there for potential future use.
+- LlamaIndex is changing all the time. By the time you see this, there may be a better
+    agent to insert. This is just using a simple VectorStore Query Engine. You may
+    want to swap that out.
+
 """
+import argparse
 import gradio as gr
 import os
+import pandas as pd
 import pinecone
-from typing import Any, Sequence
+from typing import Any, Sequence, Tuple
 
 from llama_index.vector_stores import PineconeVectorStore
-from llama_index import VectorStoreIndex
+from llama_index import VectorStoreIndex, ServiceContext
+from llama_index.llms import OpenAI
 
 """
 USER CONFIG
@@ -28,8 +40,8 @@ PINECONE_NAMESPACE = None
 # Pinecone environment to use
 PINECONE_ENV_NAME = "us-east1-gcp"
 
-# Embedding model to use, must match that used in index
-EMBEDDING_MODEL = "text-embedding-ada-002"
+# Which language model to use. See OpenAPI docs for options
+LANGUAGE_MODEL = "gpt-3.5-turbo-16k"
 
 # Number of retrieved sources to pass in to the LLM
 TOP_K = 5
@@ -39,6 +51,23 @@ MAX_TEXT_PRINT = 1000
 
 # Flag to show sources or not
 SHOW_SOURCES = True
+
+# Table headers
+TABLE_HEADERS = ("Source", "Channel", "Title", "URL", "Snippet")
+
+# Base prompt to hack together conversational history
+BASE_PROMPT = """
+Given the following conversation history, what is the answer to the the question
+posed at the bottom?
+
+The history is in the format, starting at <HISTORY>:
+Interaction #[i]:
+Q: Question
+A: Answer
+
+<HISTORY>:
+
+"""
 
 """
 INITIALIZE
@@ -57,19 +86,25 @@ vector_store = PineconeVectorStore(
 )
 
 # Build index from existing vector store
-index = VectorStoreIndex.from_vector_store(
-    vector_store,  # vector_store_info=metadata_fields
-)
+index = VectorStoreIndex.from_vector_store(vector_store)
+
+# Create language model and bind to service context
+gpt_model = OpenAI(temperature=0, model=LANGUAGE_MODEL)
+service_context_gpt = ServiceContext.from_defaults(llm=gpt_model)
 
 # Create engine
-query_engine = index.as_query_engine(similarity_top_k=TOP_K)
+query_engine = index.as_query_engine(
+    service_context=service_context_gpt, similarity_top_k=TOP_K
+)
 
 """
 FUNCTIONS AND START
 """
 
 
-def ask_question(message: str, history: Sequence[Any]) -> str:
+def ask_question(
+    message: str, history: Sequence[Any]
+) -> Tuple[str, Sequence[str], pd.DataFrame]:
     """
     Ask a question and return the response
 
@@ -78,50 +113,121 @@ def ask_question(message: str, history: Sequence[Any]) -> str:
         history (Sequence[Any]): The history of the conversation
 
     Returns:
-        str: The formatted response
+        response: The formatted response
+        history: The updated history
+        source_data: The dataframe of sources
+
     """
-    response = query_engine.query(message)
+    # Build a single message to query using conversational history
+    query = ""
+    if history:
+        query = BASE_PROMPT
+        for i, (msg, resp) in enumerate(history, 1):
+            query += f"Interaction #{i}:\nQ: {msg}\nA: {resp}\n\n"
 
-    output = "\nResponse:\n---\n"
-    output += str(response)
+    # Add the question to the end
+    query += f"Q: {message}\nA:"
 
-    # Get sources
-    output += "\nSources:\n---\n"
-    nodes = response.source_nodes
+    # Ask the question
+    query_response = query_engine.query(query)
+
+    # Parse the raw reply
+    response = str(query_response)
+
+    # Extract the sources
+    source_data = pd.DataFrame(columns=TABLE_HEADERS)
+    nodes = query_response.source_nodes
     for i, node in enumerate(nodes, 1):
         # Get shortened source text
         source_text = node.node.text[2:MAX_TEXT_PRINT]
 
         # Get metadata
         metadata = node.node.extra_info
-        metadata.pop("url_base", None)
-        metadata.pop("n_tokens", None)
-        metadata.pop("duration", None)
 
-        # pretty print
-        output += f"Source {i}:\n"
-        output += f"Text: {source_text}\n"
-        output += "Metadata:\n"
-        for k, v in metadata.items():
-            output += f"\t{k}: {v}\n"
-        output += "\n"
+        # add a row to dataframe
+        source_data.loc[i] = [
+            metadata.get("source"),
+            metadata.get("channel"),
+            metadata.get("title"),
+            metadata.get("url"),
+            source_text,
+        ]
 
     # append history
-    history.append((message, output))
+    history.append((message, response))
 
-    return output
+    # Print for logging purposes
+    print(history)
+
+    return response, history, source_data
 
 
-with gr.Blocks() as demo:
-    chatbot = gr.Chatbot()
+with gr.Blocks() as ui:
+    # Add input textbox
     msg = gr.Textbox()
-    clear = gr.ClearButton([msg, chatbot])
 
-    msg.submit(ask_question, [msg, chatbot], [msg, chatbot])
+    # Add a Chatbot like interface to make it look pretty.
+    chatbot = gr.Chatbot()
+
+    # add hidden textbox just to dump output
+    msg_hidden = gr.Textbox(visible=False)
+
+    # Create a row of buttons
+    with gr.Row():
+        submit = gr.Button("Submit")
+        clear = gr.ClearButton([msg, chatbot])
+
+    # Add a table to show sources after the query
+    table = gr.DataFrame(headers=TABLE_HEADERS, row_count=(TOP_K, "fixed"), wrap=True)
+
+    # Set up the click event for the submit button
+    submit.click(
+        ask_question, inputs=[msg, chatbot], outputs=[msg_hidden, chatbot, table]
+    )
+
+    # Alternately, allow pressing enter on textbox to submit too
+    msg.submit(
+        ask_question, inputs=[msg, chatbot], outputs=[msg_hidden, chatbot, table]
+    )
 
 if __name__ == "__main__":
-    demo.launch()
+    # Add a address string argument that defaults to 127.0.0.1
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--address",
+        type=str,
+        default="127.0.0.1",
+        help="""
+        Address to run the server on. 127.0.0.1 for local. 0.0.0.0 for "
+        remote or docker
+        """,
+    )
+    # add a port with None default
+    parser.add_argument(
+        "--port",
+        type=int,
+        default=None,
+        help="Port to run the server on",
+    )
+    parser.add_argument(
+        "--username",
+        type=str,
+        default=None,
+        help="Username for basic auth",
+    )
+    parser.add_argument(
+        "--password",
+        type=str,
+        default=None,
+        help="Password for basic auth",
+    )
+    args = parser.parse_args()
 
+    # Configure auth
+    if args.username and args.password:
+        auth = (args.username, args.password)
+    else:
+        auth = None
 
-# Start the app
-gr.ChatInterface(ask_question).launch()
+    # Launch UI
+    ui.launch(server_name=args.address, server_port=args.port, auth=auth)
