87c445bc36f762af85d30e533eca065d50b5a901
Tom Szumowski <tszumowski@gmail.com>
2023-08-21 21:15:04 -0400

    update config

--- cloud_functions/embedding-indexer/config.py
+++ cloud_functions/embedding-indexer/config.py
@@ -7,4 +7,4 @@ PINECONE_INDEX_NAME = "openai-embedding-index"  # Get in Pinecone console
 PINECONE_ENV_NAME = "us-east1-gcp"  # Get next to API key in Pinecone console
 MAX_TOKENS_INPUT = 550  # Max number of tokens from input before chunking
 TOKEN_CHUNK_SIZE = 450  # Number of tokens to chunk if input is too long
-BUCKET_PATH = "gs://liquid-champion/embedding-indexer"  # bucket path to backup vector
+BUCKET_PATH = "gs://[BUCKET]/embedding-indexer"  # bucket path to backup vector
